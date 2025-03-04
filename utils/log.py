db2f512744ecf938109ac4064163447b42fe6497
Potential <1317285046@qq.com>
2025-03-04 06:50:42 +0000

    Modify log bug

--- utils/log.py
+++ utils/log.py
@@ -1,8 +1,13 @@
+import os
 import logging
 from config import CONFIG_YAML
 
 log_level = CONFIG_YAML["LOGGER"]["level"]
-log_file = CONFIG_YAML["LOGGER"]["dir"] + CONFIG_YAML["LOGGER"]["file"]
+log_dir = CONFIG_YAML["LOGGER"]["dir"]
+log_file = log_dir + CONFIG_YAML["LOGGER"]["file"]
+
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
 
 handler = logging.FileHandler(log_file)
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
