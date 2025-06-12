ca14e8d6673f32046dff71a199d7e537695a7d82
scener-y <1838648927@qq.com>
2025-06-12 17:59:16 +0800

    添加docker目录，添加一个依赖包

---
+++ docker/run_docker.sh
@@ -0,0 +1,4 @@
+#sudo docker build -t neotech_mrna_prediction_agent:v0.1 .
+
+#sudo docker run -itd --name stg_mrna_prediction_agent --net=host -v /mnt/workspace/stg:/opt/workspace/neotech bdf1a184ab10 bash
+
