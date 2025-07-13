9b7c981b8068d19ff5aecf4a33e388dab2e7ee87
scener-y <1838648927@qq.com>
2025-07-13 12:51:24 +0800

    添加启动命令

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-nohup uvicorn app:app  --host 0.0.0.0 --port 60718 1>log 2>err &
+nohup uvicorn app:app \
+  --timeout-keep-alive 21600 \
+  --timeout-graceful-shutdown 1200 \
+  --host 0.0.0.0 \
+  --port 60718 \
+  >>log 2>>err &
